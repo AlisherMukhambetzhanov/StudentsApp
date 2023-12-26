68b0bb08007338a2267915fe5afb071455699343
Alisher <alishermail111@gmail.com>
2023-12-26 19:04:12 +0600

    Modified dockerfile with tables creation

---
+++ init_db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    middle_name TEXT,
+    birth_date TEXT,
+    group_name TEXT
+);
