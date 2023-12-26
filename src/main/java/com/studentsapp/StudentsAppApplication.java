29dafb122b40ef3df7c04e1e93ba081ebd1501e7
Alisher <alishermail111@gmail.com>
2023-12-26 19:28:15 +0600

    Added pom.xml and main class

---
+++ src/main/java/com/studentsapp/StudentsAppApplication.java
@@ -0,0 +1,12 @@
+package com.studentsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsAppApplication.class, args);
+    }
+}
